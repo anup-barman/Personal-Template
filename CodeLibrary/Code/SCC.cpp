0e2102f4bb07bcde831af2b32877e63132bebabf
Anup Barman <183620611+anup-barman@users.noreply.github.com>
2026-04-06 15:35:42 +0600

    Add legendre's formula

--- CodeLibrary/Code/SCC.cpp
+++ CodeLibrary/Code/SCC.cpp
@@ -1,68 +1,59 @@
-vector<int> st;    // - stack holding the unclaimed vertices
-vector<int> roots; // - keeps track of the SCC roots of the vertices
-int timer;         // - dfs timestamp counter
-vector<int> t_in;  // - keeps track of the dfs timestamp of the vertices
-vector<int> t_low; // - keeps track of the lowest t_in of unclaimed vertices
-                   // reachable in the subtree
+vector<bool> visited; // keeps track of which vertices are already visited
 
-// implements the tarjan algorithm for strongly connected components
-void dfs(int v, vector<vector<int>> const &adj, vector<vector<int>> &components) {
-
-  t_low[v] = t_in[v] = timer++;
-  st.push_back(v);
-
-  for (auto u : adj[v]) {
-    if (t_in[u] == -1) { // tree-edge
-      dfs(u, adj, components);
-      t_low[v] = min(t_low[v], t_low[u]);
-    } else if (roots[u] == -1) { // back-edge, cross-edge or forward-edge to an unclaimed vertex
-      t_low[v] = min(t_low[v], t_in[u]);
-    }
-  }
-
-  if (t_low[v] == t_in[v]) { // vertex is a root
-    components.push_back({v}); // initializes a new component with root v
-    while (true) {
-      int u = st.back();
-      st.pop_back();
-      roots[u] = v; // claims the vertex
-      if (u == v)
-        break;
-      components.back().push_back(u); // adds vertex u to the component of v
-    }
-  }
+// runs depth first search starting at vertex v.
+// each visited vertex is appended to the output vector when dfs leaves it.
+void dfs(int v, vector<vector<int>> const& adj, vector<int> &output) {
+    visited[v] = true;
+    for (auto u : adj[v])
+        if (!visited[u])
+            dfs(u, adj, output);
+    output.push_back(v);
 }
 
 // input: adj -- adjacency list of G
 // output: components -- the strongy connected components in G
 // output: adj_cond -- adjacency list of G^SCC (by root vertices)
-void strongly_connected_components(vector<vector<int>> const &adj,
-                                   vector<vector<int>> &components,
-                                   vector<vector<int>> &adj_cond) {
-  components.clear();
-  adj_cond.clear();
+void strongly_connected_components(vector<vector<int>> const& adj,
+                                  vector<vector<int>> &components,
+                                  vector<vector<int>> &adj_cond) {
+    int n = adj.size();
+    components.clear(), adj_cond.clear();
 
-  int n = adj.size();
+    vector<int> order; // will be a sorted list of G's vertices by exit time
 
-  st.clear();
-  roots.assign(n, -1);
-  timer = 0;
-  t_in.assign(n, -1);
-  t_low.assign(n, -1);
+    visited.assign(n, false);
 
-  // applies the tarjan algorithm to all the vertices
-  // adds vertices to the components in reverse topological order
-  for (int v = 0; v < n; v++) {
-    if (t_in[v] == -1) {
-      dfs(v, adj, components);
-    }
-  }
+    // first series of depth first searches
+    for (int i = 0; i < n; i++)
+        if (!visited[i])
+            dfs(i, adj, order);
 
-  // adds edges to the condensation graph
-  adj_cond.assign(n, {});
-  for (int v = 0; v < n; v++) {
-    for (auto u : adj[v])
-      if (roots[v] != roots[u])
-        adj_cond[roots[v]].push_back(roots[u]);
-  }
+    // create adjacency list of G^T
+    vector<vector<int>> adj_rev(n);
+    for (int v = 0; v < n; v++)
+        for (int u : adj[v])
+            adj_rev[u].push_back(v);
+
+    visited.assign(n, false);
+    reverse(order.begin(), order.end());
+
+    vector<int> roots(n, 0); // gives the root vertex of a vertex's SCC
+
+    // second series of depth first searches
+    for (auto v : order)
+        if (!visited[v]) {
+            std::vector<int> component;
+            dfs(v, adj_rev, component);
+            components.push_back(component);
+            int root = *component.begin();
+            for (auto u : component)
+                roots[u] = root;
+        }
+
+    // add edges to condensation graph
+    adj_cond.assign(n, {});
+    for (int v = 0; v < n; v++)
+        for (auto u : adj[v])
+            if (roots[v] != roots[u])
+                adj_cond[roots[v]].push_back(roots[u]);
 }
