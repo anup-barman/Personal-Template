7299b94722f3fede0e0c78ec830dbc7e10b748f7
Anup Barman <183620611+anup-barman@users.noreply.github.com>
2026-04-13 23:15:08 +0600

    renamed some cpp files

--- CodeLibrary/Code/Sparse-Table.cpp
+++ CodeLibrary/Code/Sparse-Table.cpp
@@ -1,18 +1,20 @@
-const int MX = 2e5 + 10;
-int n, arr[MX], st[25][MX];
+const int MAXN = 2e5 + 10;
+const int K = 26;
+
+int n, arr[MX], spt[25][MX];
 int log2Floor(int i) {
   return 31 - __builtin_clz(i);
 }
 void build() {
   int k = log2Floor(n);
-  copy(arr, arr + n, st[0]);
+  copy(arr, arr + n, spt[0]);
   for (int i = 1; i <= k; ++i) {
     for (int j = 0; j + (1 << i) <= n; j++) {
-      st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+      spt[i][j] = min(spt[i - 1][j], spt[i - 1][j + (1 << (i - 1))]);
     }
   }
 }
 int query(int l, int r) {
   int i = log2Floor(r - l + 1);
-  return min(st[i][l], st[i][r - (1 << i) + 1]);
+  return min(spt[i][l], spt[i][r - (1 << i) + 1]);
 }
