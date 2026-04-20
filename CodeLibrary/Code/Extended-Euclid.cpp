097e156da941f0bcd0b9c2f280559800f7f38a3c
Anup Barman <183620611+anup-barman@users.noreply.github.com>
2026-04-20 23:07:32 +0600

    update

--- CodeLibrary/Code/Extended-Euclid.cpp
+++ CodeLibrary/Code/Extended-Euclid.cpp
@@ -1,6 +1,6 @@
-// It finds integers x and y (coefficients) such that: ax + by = gcd(a, b)
+// Finds the greatest common divisor of a and b.
+// Also finds integers x and y such that: ax + by = gcd(a, b)
 int extendedGCD(int a, int b, int& x, int& y) {
-  // Base Case
   if (a == 0) {
     x = 0;
     y = 1;
@@ -8,7 +8,6 @@ int extendedGCD(int a, int b, int& x, int& y) {
   }
   int x1, y1;
   int g = extendedGCD(b % a, a, x1, y1);
-  // Update x and y using results of recursive call
   x = y1 - (b / a) * x1;
   y = x1;
   return g;
