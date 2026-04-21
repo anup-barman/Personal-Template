be49e160ee612a950f0b0c1e44d814b627677cc1
Anup Barman <183620611+anup-barman@users.noreply.github.com>
2026-04-21 13:16:29 +0600

    update

--- CodeLibrary/Code/Sparse-Table.cpp
+++ CodeLibrary/Code/Sparse-Table.cpp
@@ -6,7 +6,6 @@ void build(int n) {
   for (int i = 0; i < n; ++i) {
     st[i][0] = arr[i];
   }
-
   for (int j = 1; j < LOG; ++j) {
     for (int i = 0; i + (1 << j) <= n; ++i) {
       st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
@@ -15,6 +14,6 @@ void build(int n) {
 }
 int query(int l, int r) {
   int len = r - l + 1;
-  int k = 31 - __builtin_popcount(len);
+  int k = __lg(len);
   return min(st[l][k], st[r - (1 << k) + 1][k]);
 }
